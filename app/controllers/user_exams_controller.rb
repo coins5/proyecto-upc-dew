42a35feb473cc73d857819ac1fd1c5ee86eb017c
Marlon Colca <marlon.colca.io@gmail.com>
2020-04-19 17:40:37 -0500

    comentarios

--- app/controllers/user_exams_controller.rb
+++ app/controllers/user_exams_controller.rb
@@ -1,49 +1,3 @@
-=begin
-class UserExamsController < ApplicationController
-  before_action :set_user_exam, only: [:show, :update, :destroy]
-
-  # GET /user_exams
-  def index
-    @user_exams = User_Exam.all
-    json_response(@user_exams)
-  end
-
-  # POST /user_exams
-  def create
-    @user_exam = User_Exam.create!(user_exam_params)
-    json_response(@user_exam, :created)
-  end
-
-  # GET /user_exams/:id
-  def show
-    json_response(@user_exam)
-  end
-
-  # PUT /user_exams/:id
-  def update
-    @user_exam.update(user_exam_params)
-    head :no_content
-  end
-
-  # DELETE /user_exams/:id
-  def destroy
-    @user_exam.destroy
-    head :no_content
-  end
-
-  private
-
-  def user_exam_params
-    # whitelist params
-    params.permit(:fecha, :puntaje, :user_id, :exam_id)
-  end
-
-  def set_user_exam
-    @user_exam = User_Exam.find(params[:id])
-  end
-end
-=end
-
 class UserExamsController < ApplicationController
   before_action :set_user
   before_action :set_user_exam, only: [:show, :update, :destroy]
@@ -87,7 +41,6 @@ class UserExamsController < ApplicationController
   end
 
   def set_user_exam
-    # @item = @user.items.find_by!(id: params[:id]) if @user
     @user_exam = @user.user_exams.find_by!(id: params[:id]) if @user
   end
 end
